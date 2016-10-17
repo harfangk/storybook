653c052342626b0fe3df3860adff9d7e7848c8ee
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2016-10-17 11:27:13 +0900

    Pass user model test
    Made model test async

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -1,30 +1,30 @@
 defmodule Storybook.UserTest do
-  use Storybook.ModelCase
+  use Storybook.ModelCase, async: true
 
   alias Storybook.User
 
-  @valid_attrs %{email: "example@example.com", name: "John Doe", username: "jdk412"}
+  @valid_attrs %{email: "example@example.com", username: "jdk412", password: "thisispassword"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
+  test "create_changeset with valid attributes" do
+    changeset = User.create_changeset(%User{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
+  test "create_changeset with invalid attributes" do
+    changeset = User.create_changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
 
-  test "changeset does not accept long usernames" do
+  test "create_changeset does not accept long usernames" do
     attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 41))
-    changeset = User.changeset(%User{}, attrs)
+    changeset = User.create_changeset(%User{}, attrs)
     assert {:username, {"should be at most %{count} character(s)", count: 40}} in changeset.errors
   end
 
-  test "registration_changeset password must be at least 6 characters long" do
-    attrs = Map.put(@valid_attrs, :password, "123456")
-    changeset = User.registration_changeset(%User{}, attrs)
+  test "create_changeset password must be at least 6 characters long" do
+    attrs = Map.put(@valid_attrs, :password, "12345")
+    changeset = User.create_changeset(%User{}, attrs)
     assert {:password, {"should be at least %{count} character(s)", count: 6}} in changeset.errors
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -14,9 +14,11 @@ defmodule Storybook.User do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
+  def create_changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :name, :username])
-    |> validate_required([:email, :name, :username])
+    |> cast(params, [:email, :username, :password])
+    |> validate_required([:email, :username, :password])
+    |> validate_length(:username, max: 40)
+    |> validate_length(:password, min: 6)
   end
 end
